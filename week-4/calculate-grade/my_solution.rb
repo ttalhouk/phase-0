11c02442af8a42903ae59729b72d8d893b969bfa
[Talal Talhouk] <[talal@sbcglobal.net]>
2016-03-03 04:38:50 +0000

    Add solutions for 4.5

--- week-4/calculate-grade/my_solution.rb
+++ week-4/calculate-grade/my_solution.rb
@@ -1,6 +1,20 @@
 # Calculate a Grade
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself, with: Denny ].
 
 
 # Your Solution Below
+def get_grade(average)
+  case
+    when average >=90
+      return "A"
+    when average >=80
+      return "B"
+    when average >=70
+      return "C"
+    when average >=60
+      return "D"
+    else
+      return "F"
+  end
+end
--- week-4/triangle-side/my_solution.rb
+++ week-4/triangle-side/my_solution.rb
@@ -1,8 +1,12 @@
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself, with: Denny].
 
 
 # Your Solution Below
 
 def valid_triangle?(a, b, c)
-  # Your code goes here!
+  if a+b>c && a+c>b && b+c>a
+    return true
+  else
+    return false
+  end
 end
